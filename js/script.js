79c916e67572b33e3ac49af4b2b2e870c6aca184
Rahul Pramanik <rprahul1616@gmail.com>
2023-04-22 15:14:50 +0530

    change into script.js dt.-22/04/2023 at.-03:14 PM

--- js/script.js
+++ js/script.js
@@ -3,6 +3,5 @@ let darkModeIcon= document.querySelector('#darkMode-icon');
 
 darkModeIcon.onclick =()=>{
     darkModeIcon.classList.toggle('bxs-sun');
-    // darkModeIcon.body.classList.toggle('darkMode-on');
     document.body.classList.toggle('darkMode-on')
 };
